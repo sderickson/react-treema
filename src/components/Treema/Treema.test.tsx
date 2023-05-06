88f4f33fc8343aa6e7cc354b4c4aea0c40ae36fe
Scott Erickson <sderickson@gmail.com>
2023-05-06 14:52:40 -0700

    Add tests, fix some things as a result

--- src/components/Treema/Treema.test.tsx
+++ src/components/Treema/Treema.test.tsx
@@ -1,9 +1,65 @@
-// import React from 'react';
-// import { render, screen, fireEvent } from '@testing-library/react';
-// import { TreemaNode } from './TreemaNode';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { TreemaNode } from './TreemaNode';
 
-describe('<TreemaNode />', () => {
-  it('should be tested?!', () => {
-    expect(true).toBe(true);
+describe('basic types', () => {
+  it('should render strings', () => {
+    render(<TreemaNode
+      data='testasdf'
+      schema={{'type': 'string'}}
+    />
+    );
+    const treema = screen.getByText('testasdf');
+    expect(treema).toBeInTheDocument();
+  });
+
+  it('should render null', () => {
+    render(<TreemaNode
+      data={null}
+      schema={{'type': 'null'}}
+    />
+    );
+    const treema = screen.getByText('null');
+    expect(treema).toBeInTheDocument();
+  });
+
+  it('should render numbers', () => {
+    render(<TreemaNode
+      data={123}
+      schema={{'type': 'number'}}
+    />
+    );
+    const treema = screen.getByText('123');
+    expect(treema).toBeInTheDocument();
+  });
+
+  it('should render boolean', () => {
+    render(<TreemaNode
+      data={true}
+      schema={{'type': 'boolean'}}
+    />
+    );
+    const treema = screen.getByText('true');
+    expect(treema).toBeInTheDocument();
+  });
+
+  it('should render arrays', () => {
+    render(<TreemaNode
+      data={[1, 234, 3]}
+      schema={{'type': 'array', 'items': {'type': 'number'}}}
+    />
+    );
+    const treema = screen.getByText('234');
+    expect(treema).toBeInTheDocument();
+  });
+
+  it('should render objects', () => {
+    render(<TreemaNode
+      data={{'a': 1, 'b': 234}}
+      schema={{'type': 'object', 'properties': {'a': {'type': 'number'}, 'b': {'type': 'number'}}}}
+    />
+    );
+    const treema = screen.getByText('234');
+    expect(treema).toBeInTheDocument();
   });
 });
--- src/components/Treema/TreemaNode.tsx
+++ src/components/Treema/TreemaNode.tsx
@@ -4,50 +4,53 @@ import './styles.css';
 export interface TreemaNodeProps {
   data: any;
   schema: SupportedJsonSchema;
+  key?: string;
 }
 
 export interface SupportedJsonSchema {
   type: 'null' | 'boolean' | 'object' | 'array' | 'number' | 'string';
-  items: SupportedJsonSchema;
-  properties: {[key: string]: SupportedJsonSchema};
+  items?: SupportedJsonSchema;
+  properties?: {[key: string]: SupportedJsonSchema};
 }
 
-const TreemaObjectNode: FC<TreemaNodeProps> = ({ data, schema }) => {
+const TreemaObjectNode: FC<TreemaNodeProps> = ({ data, schema, key }) => {
   const propSchemas = schema.properties;
+  if (!propSchemas) return null;
   const children = Object.keys(data).map((key: string) => {
     const propSchema = propSchemas[key];
     const component = typeMapping[propSchema.type];
-    return component({ data: data[key], schema: propSchema });
+    return component({ data: data[key], schema: propSchema, key: key });
   }).filter((e) => e);
-  return <TreemaNodeLayout display={<span>this is an object</span>} open={true}>
+  return <TreemaNodeLayout display={<span>this is an object</span>} open={true} key={key}>
     <>{children}</>
   </TreemaNodeLayout>;
 };
 
 const TreemaArrayNode: FC<TreemaNodeProps> = ({ data, schema }) => {
   const itemSchema = schema.items;
+  if (!itemSchema) return null;
   const component = typeMapping[itemSchema.type];
   return <TreemaNodeLayout display={<span>this is an array</span>} open={true}>
-    {data.map((item: any) => {
-      return component({ data: item, schema: itemSchema });
+    {data.map((item: any, index: number) => {
+      return component({ data: item, schema: itemSchema, key: index+'' });
     })}
   </TreemaNodeLayout>;
 }
 
-const TreemaStringNode: FC<TreemaNodeProps> = ({ data, schema }) => {
-  return TreemaNodeLayout({ display: <span>{data}</span> });
+const TreemaStringNode: FC<TreemaNodeProps> = ({ data, schema, key }) => {
+  return TreemaNodeLayout({ display: <span>{data}</span>, key: key });
 }
 
-const TreemaNumberNode: FC<TreemaNodeProps> = ({ data, schema }) => {
-  return TreemaNodeLayout({ display: <span>{data}</span> });
+const TreemaNumberNode: FC<TreemaNodeProps> = ({ data, schema, key }) => {
+  return TreemaNodeLayout({ display: <span>{data}</span>, key: key });
 }
 
-const TreemaBooleanNode: FC<TreemaNodeProps> = ({ data, schema }) => {
-  return TreemaNodeLayout({ display: <span>{data}</span> });
+const TreemaBooleanNode: FC<TreemaNodeProps> = ({ data, schema, key }) => {
+  return TreemaNodeLayout({ display: <span>{JSON.stringify(data)}</span>, key: key });
 }
 
-const TreemaNullNode: FC<TreemaNodeProps> = ({ data, schema }) => {
-  return TreemaNodeLayout({ display: <span>null</span> });
+const TreemaNullNode: FC<TreemaNodeProps> = ({key}) => {
+  return TreemaNodeLayout({ display: <span>null</span>, key: key });
 }
 
 const typeMapping: {[key: string]: FC<TreemaNodeProps>} = {
@@ -63,11 +66,12 @@ export interface TreemaNodeLayoutProps {
   display: ReactNode;
   open?: boolean;
   children?: JSX.Element,
+  key?: string;
 }
 
-export const TreemaNodeLayout: FC<TreemaNodeLayoutProps> = ({ open, display, children }) => {
+export const TreemaNodeLayout: FC<TreemaNodeLayoutProps> = ({ open, display, children, key }) => {
   // stateless, common way to layout treema nodes generally. Should not include any schema specific logic.
-  return <div className="treema-node">
+  return <div className="treema-node" key={key}>
     {open !== undefined && open !== null && <span className="treema-toggle">{open ? 'O' : 'X' }</span>}
     {display}
     {children ? <div className="treema-children">{children}</div> : null}
