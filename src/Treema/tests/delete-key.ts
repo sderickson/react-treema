5214ed99c0de383afc3118a70e25476bca82f00f
Scott Erickson <sderickson@gmail.com>
2023-08-13 16:08:49 -0700

    Extend delete key tests

--- src/Treema/tests/delete-key.ts
+++ src/Treema/tests/delete-key.ts
@@ -26,7 +26,7 @@ export const deleteAllData: GenericTest = {
 };
 
 export const deleteMetaSelect: GenericTest = {
-  name: 'deletes all entries in an array selected with meta/ctrl + click',
+  name: 'deletes all entries in an array selected with meta/ctrl + click, handles undo',
   test: async (ctx) => {
     await ctx.fireFocus();
     await ctx.keyboard("{Meta>}");
@@ -35,11 +35,18 @@ export const deleteMetaSelect: GenericTest = {
     await ctx.keyboard("{/Meta}");
     await ctx.fireBackspace();
     ctx.expect(ctx.getData()).toEqual([2]);
+    ctx.expect(ctx.getAllSelected()).toEqual({'/0': true});
+    await ctx.fireUndo();
+    ctx.expect(ctx.getData()).toEqual([1,2,3]);
+    ctx.expect(ctx.getAllSelected()).toEqual({'/0': true, '/2': true});
+    await ctx.fireRedo();
+    ctx.expect(ctx.getData()).toEqual([2]);
+    ctx.expect(ctx.getAllSelected()).toEqual({'/0': true});
   }
 }
 
 export const deleteShiftSelect: GenericTest = {
-  name: 'deletes all entries in an array selected with shift + click',
+  name: 'deletes all entries in an array selected with shift + click, handles undo',
   test: async (ctx) => {
     await ctx.fireFocus();
     await ctx.keyboard("{Shift>}");
@@ -47,6 +54,13 @@ export const deleteShiftSelect: GenericTest = {
     await ctx.click(ctx.query().getByTestId('/2'));
     await ctx.keyboard("{/Shift}");
     await ctx.fireBackspace();
+    ctx.expect(ctx.getAllSelected()).toEqual({'': true});
+    ctx.expect(ctx.getData()).toEqual([]);
+    await ctx.fireUndo();
+    ctx.expect(ctx.getAllSelected()).toEqual({ '/0': true, '/1': true, '/2': true });
+    ctx.expect(ctx.getData()).toEqual([1,2,3]);
+    await ctx.fireRedo();
+    ctx.expect(ctx.getAllSelected()).toEqual({'': true});
     ctx.expect(ctx.getData()).toEqual([]);
   }
 }
